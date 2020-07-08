6dec9e59795857eaa7fc28274b3c0a8089a05c97
Jacob Dahan <45498594+JacobDahan@users.noreply.github.com>
2020-07-08 00:03:13 -0700

    Updated formatting

--- whisker2joints.py
+++ whisker2joints.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 def convert_whisker_to_joint_labels(x_coords, y_coords, n_joints):
-
-    distance = np.cumsum(np.sqrt( np.ediff1d(x_coords, to_begin=0)**2 + np.ediff1d(y_coords, to_begin=0)**2 ))
-    distance = distance/distance[-1]
-
-    fx, fy = interp1d( distance, x_coords ), interp1d( distance, y_coords )
-
-    alpha                = np.linspace(0, 1, n_joints)
-    x_regular, y_regular = fx(alpha), fy(alpha)
-
-    coords = zip(y_regular, x_regular)
-    df     = pandas.DataFrame(coords, columns=('y', 'x'))
-
+    """
+    Converts x- and y-coordinates into n equidistant joints.
+    Returns df of n rows with columns y and x containing joint coordinates.
+    """
+    distances             = np.cumsum(np.sqrt( np.ediff1d(x_coords, to_begin=0)**2 + np.ediff1d(y_coords, to_begin=0)**2 ))
+    point2point_distances = distances/distances[-1]
+    fx, fy                = interp1d( point2point_distances, x_coords ), interp1d( point2point_distances, y_coords )
+    alpha                 = np.linspace(0, 1, n_joints)
+    x_out, y_out          = fx(alpha), fy(alpha)
+    coords                = zip(x_out, y_out)
+    df                    = pandas.DataFrame(coords, columns=('y', 'x'))
     return df
